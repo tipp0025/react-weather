e2cf68ec7721f48f60ada6891c706174bfa18e78
tipp0025 <tipp0025@algonquinlive.com>
2025-09-02 14:27:26 -0400

    Added FeedbackBar component for error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useTheme } from "./hooks/useTheme";
 import "./App.css";
 import Header from "./components/Header/Header";
@@ -9,6 +10,7 @@ import Weather from "./components/Weather/Weather";
 
 function App() {
   const { theme, toggleTheme } = useTheme();
+  const [errorMessage, setErrorMessage] = useState("");
 
   const onSearch = (result) => {
     if (result.error) {
@@ -20,6 +22,9 @@ function App() {
       setErrorMessage("");
     }
   };
+  const clearErrorMessage = () => {
+    setErrorMessage("");
+  };
 
   return (
     <>
@@ -27,7 +32,10 @@ function App() {
         <Header theme={theme} onToggleTheme={toggleTheme} />
         <main className="main">
           <SearchBar onSearch={onSearch} />
-          <FeedbackBar />
+          <FeedbackBar
+            message={errorMessage}
+            clearMessage={clearErrorMessage}
+          />
           <LocationBar>
             <LocationCard />
           </LocationBar>
--- src/components/FeedbackBar/FeedbackBar.jsx
+++ src/components/FeedbackBar/FeedbackBar.jsx
@@ -1,9 +1,26 @@
-function FeedbackBar() {
-  return (
-    <div>
-      <p>Errors displayed here</p>
-    </div>
-  );
+import { useEffect } from "react";
+import PropTypes from "prop-types";
+import "./FeedbackBar.css";
+
+function FeedbackBar({ message, clearMessage }) {
+  useEffect(() => {
+    if (!message) return;
+
+    const timer = setTimeout(() => {
+      clearMessage();
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [message, clearMessage]);
+
+  if (!message) return null;
+
+  return <div className="feedback-bar">{message}</div>;
 }
+
+FeedbackBar.propTypes = {
+  message: PropTypes.string,
+  clearMessage: PropTypes.func.isRequired,
+};
 
 export default FeedbackBar;
