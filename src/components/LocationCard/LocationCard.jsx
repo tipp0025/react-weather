fcdaeab911f8dadc763856449140513b1f3750d4
tipp0025 <tipp0025@algonquinlive.com>
2025-09-04 19:00:12 -0400

    Updated Location Bar/Card styling

--- src/components/LocationCard/LocationCard.jsx
+++ src/components/LocationCard/LocationCard.jsx
@@ -3,18 +3,22 @@ import "./LocationCard.css";
 
 function LocationCard({ data, onRemove, onClick }) {
   return (
-    <div className="location-card" onClick={onClick}>
-      <h2>{data?.name}</h2>
-      <p>{data?.sys?.country}</p>
+    <button className="location-card" onClick={onClick} type="button">
+      <h2 className="card-title">{data?.name}</h2>
+      <p className="card-subtitle">{data?.sys?.country}</p>
       <button
+        className="card-remove"
+        type="button"
         onClick={(e) => {
           e.stopPropagation();
           onRemove();
         }}
+        aria-label={`Remove ${data?.name}`}
+        title="Remove"
       >
         Remove
       </button>
-    </div>
+    </button>
   );
 }
 LocationCard.propTypes = {
